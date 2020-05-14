15ea48cbd9274ae06057b015f66743b801556bbd
Marcos <contato.marcosvmoreira@gmail.com>
2020-05-14 10:00:38 -0300

    Add: subscriber application

---
+++ src/main/java/com/learningrabbitmq/demo/LearningRabbitApplication.java
@@ -0,0 +1,13 @@
+package com.learningrabbitmq.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningRabbitApplication {
+
+    public static void main (String[] args) {
+        SpringApplication.run(LearningRabbitApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/learningrabbitmq/demo/RabbitListener.java
@@ -0,0 +1,15 @@
+package com.learningrabbitmq.demo;
+
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.messaging.Sink;
+
+@EnableBinding(Sink.class)
+public class RabbitListener {
+
+    @StreamListener(Sink.INPUT)
+    public void handleMessage(Message message) {
+        System.out.println("Received message is: "+message);
+    }
+
+}
